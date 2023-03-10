751bdd7f990e42889e9577eb3b437a49a8d62e02
Galedrim <84284891+Galedrim@users.noreply.github.com>
2023-03-10 14:30:49 +0100

    :lipstick: Change style to something more blue neutral, add Conda environnement.

--- user_profile.ps1
+++ user_profile.ps1
@@ -10,7 +10,7 @@ Import-Module devtoolbox
 # PSReadLine\
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
+#Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
 # Fzf
