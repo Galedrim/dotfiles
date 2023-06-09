f406c755f78774710b4ede87eb8f3b5fca3417ad
Marin Paul <marinpaul03@gmail.com>
2023-06-09 18:19:00 +0200

    🔥 Delete Unused Alias and Function 🐛 Fix color problem for GIT

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,13 +1,14 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-$ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\pastel-powerline.toml"
+# Startship Prompt
+$env:STARSHIP_CONFIG = "$HOME\.config\powershell\pastel-powerline.toml"
 Invoke-Expression (& starship init powershell)
 
+# Terminal Icons
 Import-Module -Name Terminal-Icons
-Import-Module devtoolbox
 
-# PSReadLine\
+# PSReadLine
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 #Set-PSReadLineOption -PredictionViewStyle ListView
@@ -17,31 +18,25 @@ Set-PSReadLineOption -EditMode Windows
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
+# Chocolatey
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
 # Env
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
-# Alias
-Set-Alias -Name vim -Value nvim
-Set-Alias ll ls
+# Env
 Set-Alias g git
-Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias c code
 
-# Utilities
+# Function
+function grep {
+  $input | out-string -stream | select-string $args
+}
+
 function which ($command) {
-    Get-Command -Name $command -ErrorAction SilentlyContinue |
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
-function update {
-  iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
-}
-
-function profilePS1 {
-  code "$HOME\.config\powershell\user_profile.ps1"
-}
-
-function starshipTOML {
-  code "$HOME\.config\powershell\pastel-powerline.toml"
 }
